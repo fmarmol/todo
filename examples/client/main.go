4aafcefa36b82693e0e1bb356f66c638ad5fb931
fmarmol <florent.marmol@gmail.com>
2017-10-19 16:21:48 +0200

    update

--- examples/client/main.go
+++ examples/client/main.go
@@ -1,17 +1,86 @@
 /*basic implementation of client*/
 package main
 
-import "flag"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	flagc "github.com/fmarmol/flag"
+	"github.com/fmarmol/todo/api"
+	"google.golang.org/grpc"
+)
 
 const (
-	LIST   string = "list"
-	CREATE        = "create"
-	UPDATE        = "update"
-	DELETE        = "delete"
+	LIST       string = "list"
+	CREATE            = "create"
+	UPDATE            = "update"
+	DELETE            = "delete"
+	LOW               = "LOW"
+	MEDIUM            = "MEDIUM"
+	HIGH              = "HIGH"
+	TODO              = "TODO"
+	INPROGRESS        = "INPROGRESS"
+	DONE              = "DONE"
 )
 
 func main() {
-	action := flag.String("action", "list", "choose action from list|create|update|delete")
+	var startDate, endDate flagc.DateFlag
+
+	host := flag.String("host", "localhost:8080", "host server")
+	action := flag.String("action", LIST, "choose action from list|create|update|delete")
 	uid := flag.Int("uid", 1, "uuid task")
+	subject := flag.String("subject", "another task", "subject of the task")
+	priority := flag.String("priority", MEDIUM, "priority of the task LOW|MEDIUM|HIGH")
+	status := flag.String("status", TODO, "status of the tasks TODO|INPROGRESS|DONE")
+	flag.Var(&startDate, "start", "start date")
+	flag.Var(&endDate, "end", "end date")
 	flag.Parse()
+
+	switch *action {
+	case LIST, CREATE, UPDATE, DELETE:
+	default:
+		flag.Usage()
+		os.Exit(2)
+	}
+	switch *priority {
+	case LOW, MEDIUM, HIGH:
+	default:
+		flag.Usage()
+		os.Exit(2)
+	}
+	conn, err := grpc.Dial(*host, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal()
+	}
+	defer conn.Close()
+
+	client := api.NewToDoApiClient(conn)
+
+	switch *action {
+	case LIST:
+		tasks, err := client.List(context.Background(), &api.Empty{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, task := range tasks.Tasks {
+			fmt.Println(task)
+		}
+	case CREATE:
+		t := &api.Task{}
+		t.Uid = int32(*uid)
+		t.Start = startDate.Unix()
+		t.End = endDate.Unix()
+		t.Subject = *subject
+		t.Status = api.Task_Status(api.Task_Status_value[*status])
+		t.Priority = api.Task_Priority(api.Task_Priority_value[*priority])
+		ret, err := client.Create(context.Background(), t)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(ret.GetType())
+	default:
+	}
 }
--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"github.com/fmarmol/todo/api"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 	"google.golang.org/grpc"
 )
 
@@ -37,19 +38,30 @@ func NewServer(cfg *ConfigDB) *Server {
 	if err != nil {
 		log.Fatalf("could not connect to db %v:%v\n", url, err)
 	}
-	if err := db.Ping(); err != nil {
-		log.Fatalf("could not ping db %v:%v\n", url, err)
+
+LOOP:
+	for i := 0; i < 3; i++ {
+		if err := db.Ping(); err != nil {
+			if i < 2 {
+				log.Println("trying to ping db:%v", url)
+				time.Sleep(time.Second)
+			} else {
+				log.Fatalf("could not ping db %v:%v\n", url, err)
+			}
+		} else {
+			break LOOP
+		}
 	}
 
 	_, err = db.Exec(
 		`CREATE TABLE IF NOT EXISTS tasks(
 			id SERIAL,
 			uid INT UNIQUE,
-			start_date TIMESTAMP,
-			end_date TIMESTAMP,
-			priority VARCHAR(10),
-			status VARCHAR(10),
-			subject TEXT
+			start_date DATE,
+			end_date DATE,
+			priority VARCHAR(10) NOT NULL,
+			status VARCHAR(10) NOT NULL,
+			subject TEXT NOT NULL
 		)`)
 	if err != nil {
 		log.Fatal("something got wrong", err)
@@ -68,11 +80,21 @@ func (s *Server) List(ctx context.Context, i *api.Empty) (*api.TaskArray, error)
 
 	ret := &api.TaskArray{Tasks: []*api.Task{}}
 	for rows.Next() {
-		var task *api.Task
-		if err := rows.Scan(task.Uid, task.Start, task.End, task.Priority, task.Status, task.Subject); err != nil {
+		var task api.Task
+		var start, end pq.NullTime
+		var priority, status string
+		if err := rows.Scan(&task.Uid, &start, &end, &priority, &status, &task.Subject); err != nil {
 			return nil, err
 		}
-		ret.Tasks = append(ret.Tasks, task)
+		if start.Valid {
+			task.Start = start.Time.Unix()
+		}
+		if end.Valid {
+			task.End = end.Time.Unix()
+		}
+		task.Status = api.Task_Status(api.Task_Status_value[status])
+		task.Priority = api.Task_Priority(api.Task_Priority_value[priority])
+		ret.Tasks = append(ret.Tasks, &task)
 	}
 	if err := rows.Close(); err != nil {
 		return nil, err
@@ -82,8 +104,13 @@ func (s *Server) List(ctx context.Context, i *api.Empty) (*api.TaskArray, error)
 
 // Create implementation
 func (s *Server) Create(ctx context.Context, t *api.Task) (*api.Error, error) {
-	_, err := s.db.Exec("INSERT FROM tasks (uid,start_date,end_date,priority, status, subject) VALUES ($1,$2,$3,$4,$5i,$6)",
-		t.Uid, t.Start, t.End, t.Priority, t.Status, t.Subject)
+	_, err := s.db.Exec("INSERT INTO tasks (uid,start_date,end_date,priority, status, subject) VALUES ($1,$2,$3,$4,$5,$6)",
+		t.Uid,
+		time.Unix(t.Start, 0),
+		time.Unix(t.End, 0),
+		api.Task_Priority_name[int32(t.Priority)],
+		api.Task_Status_name[int32(t.Status)],
+		t.Subject)
 	if err != nil {
 		return &api.Error{Type: api.Error_FAIL, Description: err.Error()}, err
 	}
